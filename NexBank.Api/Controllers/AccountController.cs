839ea804e2ee4caa4a42aa41aa82ba4e7a1f4167
leandro picoli <leandropicoli@live.com>
2021-01-16 18:07:22 -0300

    Inicio do projeto e implementaçao do Account Context

---
+++ NexBank.Api/Controllers/AccountController.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using NexBank.Domain.Commands;
+using NexBank.Domain.Commands.AccountCommands;
+using NexBank.Domain.Entities;
+using NexBank.Domain.Handlers;
+using NexBank.Domain.Repositories;
+
+namespace NexBank.Api.Controllers
+{
+    [ApiController]
+    [Route("v1/account")]
+    public class AccountController : ControllerBase
+    {
+        private readonly AccountHandler _handler;
+        private readonly IAccountRepository _accountRepository;
+
+        public AccountController(AccountHandler handler, IAccountRepository accountRepository)
+        {
+            _handler = handler;
+            _accountRepository = accountRepository;
+        }
+
+        [Route("")]
+        [HttpPost]
+        public GenericCommandResult CreateAccount(CreateAccountCommand createAccountCommand)
+        {
+            return (GenericCommandResult)_handler.Handle(createAccountCommand);
+        }
+
+        [Route("getAll")]
+        [HttpGet]
+        public IEnumerable<Account> GetAllAccounts()
+        {
+            return _accountRepository.GetAll();
+        }
+
+        [Route("getById")]
+        [HttpGet]
+        public Account GetById(Guid id)
+        {
+            return _accountRepository.GetById(id);
+        }
+
+    }
+}
---
+++ NexBank.Api/Startup.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using NexBank.Infra.Contexts;
+using Microsoft.OpenApi.Models;
+using NexBank.Domain.Repositories;
+using NexBank.Infra.Repositories;
+using NexBank.Domain.Handlers;
+
+namespace NexBank.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddDbContext<DataContext>(opt => opt.UseInMemoryDatabase("Database"));
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "NexBank", Version = "v1" });
+            });
+
+            services.AddTransient<IAccountRepository, AccountRepository>();
+            services.AddTransient<AccountHandler, AccountHandler>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseCors(x => x
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Nex Bank V1");
+            });
+        }
+    }
+}
